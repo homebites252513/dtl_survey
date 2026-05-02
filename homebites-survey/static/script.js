b1e598239692f9e1da12fe93e4c1d8a1af10f05d
homebites252513 <homebites252513@gmail.com>
2026-05-02 12:30:10 +0530

    Update script.js

--- homebites-survey/static/script.js
+++ homebites-survey/static/script.js
@@ -1,23 +1,56 @@
-function startSurvey() {
-  document.getElementById("intro").style.display = "none";
-  document.getElementById("surveyForm").style.display = "block";
-}
+let current = -1;
 
+const steps = document.querySelectorAll(".step");
+const intro = document.getElementById("intro");
 const form = document.getElementById("surveyForm");
 
-const GOOGLE_FORM_ACTION_URL =
-"https://docs.google.com/forms/d/e/1FAIpQLSfmjdUe8BvpP1nnZOARAPcK7RJbSj0f0KXlUCHQe7mXhdz-qw/formResponse";
+window.onload = () => {
+  steps.forEach(s => s.style.display = "none");
+};
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+function startSurvey() {
+  intro.style.display = "none";
+  form.style.display = "block";
+  current = 0;
+  showStep(current);
+}
 
-  const formData = new FormData(form);
-
-  await fetch(GOOGLE_FORM_ACTION_URL, {
-    method: "POST",
-    mode: "no-cors",
-    body: formData
+function showStep(i) {
+  steps.forEach((s, index) => {
+    s.style.display = index === i ? "block" : "none";
   });
 
-  alert("Submitted successfully 🚀");
-});
+  document.getElementById("nextBtn").innerText =
+    i === steps.length - 1 ? "Submit" : "Next";
+}
+
+function nextStep() {
+  if (current < steps.length - 1) {
+    current++;
+    showStep(current);
+  } else {
+    submitForm();
+  }
+}
+
+function prevStep() {
+  if (current > 0) {
+    current--;
+    showStep(current);
+  }
+}
+
+async function submitForm() {
+  const formData = new FormData(form);
+
+  await fetch(
+    "https://docs.google.com/forms/d/e/1FAIpQLSfmjdUe8BvpP1nnZOARAPcK7RJbSj0f0KXlUCHQe7mXhdz-qw/formResponse",
+    {
+      method: "POST",
+      mode: "no-cors",
+      body: formData
+    }
+  );
+
+  alert("Submitted 🚀");
+}
